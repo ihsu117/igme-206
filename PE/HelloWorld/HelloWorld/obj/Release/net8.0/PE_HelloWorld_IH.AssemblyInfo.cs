cc7df5fa32cae85e54c91de037e7cf5aa6ee3a68
Ivan Hsu (RIT Student) <ih6604@rit.edu>
2024-09-03 16:13:24 -0400

    Statements Expressions PE

--- PE/HelloWorld/HelloWorld/obj/Release/net8.0/PE_HelloWorld_IH.AssemblyInfo.cs
+++ PE/HelloWorld/HelloWorld/obj/Release/net8.0/PE_HelloWorld_IH.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PE_HelloWorld_IH")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58377b1f1edc75af27e7c89cd13c4a46a04e32c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PE_HelloWorld_IH")]
---
+++ PE/StatementsExpressions/StatementsExpressions/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Ivan Hsu
+ * PE - Statements & Expressions
+ * 
+ */
+
+//Namespace declaration
+namespace StatementsExpressions
+{
+    //Class declaration
+    internal class Program
+    {
+        //Main
+        static void Main(string[] args)
+        {
+            //Print name & label
+            Console.WriteLine("Name: Dexter Morgan");
+
+            //First stat + newline operator | 20% of total
+            Console.WriteLine("\nDexterity: " + (50 * .2));
+
+            //Second stat | Half of first stat
+            Console.WriteLine("Charisma " + (50 * .2)/2);
+
+            //Third stat | Just 7
+            Console.WriteLine("Health: " + 7);
+
+            //Fourth stat | Sum of 2nd & 1st stat minus 2
+            Console.WriteLine("Strength: " + (((50 * .2)/2) + 7 - 2));
+
+            //Fifth stat | Remainder of total after subtracting the sum of stats 1 - 4
+            Console.WriteLine("Intelligence: " +
+                (
+                   50 - (
+                        (50 * .2) +
+                        ((50 * .2) / 2) + 7 +
+                        (((50 * .2)/2) + 7 - 2)
+                   )
+                )
+            );
+
+            Console.WriteLine("\nTOTAL: 50");
+
+        }
+    }
+}
