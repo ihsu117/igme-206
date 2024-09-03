58377b1f1edc75af27e7c89cd13c4a46a04e32c4
ih6604 <ivanhsu117@gmail.com>
2024-09-03 15:57:33 -0400

    PE + Hello World

---
+++ PE/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Ivan H
+ * PE - Hello World
+ * 
+ */
+
+//Namespace declaration
+namespace HelloWorld
+{
+    //Class declaration, simple "Program"
+    internal class Program
+    {
+        //Almighty Main!
+        static void Main(string[] args)
+        {
+            //Hello world!!
+            Console.WriteLine("Hello GDP1");
+
+            Console.WriteLine("A second call!");
+
+            Console.WriteLine("My name is Ivan Hsu");
+        }
+    }
+}
